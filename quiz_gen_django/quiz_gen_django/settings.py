089baeb5073907960f51c9151b006e5fb460500c
Kovalevaolya <kovaleva.olga.2004@gmail.com>
2024-12-11 21:01:48 +0300

    Добавление скрытия секретного ключа

--- quiz_gen_django/quiz_gen_django/settings.py
+++ quiz_gen_django/quiz_gen_django/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d4%@80kt(3c4xs+-(yew3#^ipj1nyi6aj$#s@ny6w!w6%t=%9l'
+# Загружаем переменные из .env файла
+load_dotenv()
+
+# Получаем SECRET_KEY
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
